0c5e5ee8997c8297c1677a9bb3d7e59bd6d5f83b
puigalex <puig.alejandro24@gmail.com>
2020-07-29 18:00:04 -0500

    error de validacion solucionado

--- train.py
+++ train.py
@@ -147,7 +147,7 @@ if __name__ == "__main__":
 
             model.seen += imgs.size(0)
 
-        if epoch % opt.evaluation_interval == 0:
+        if epoch % opt.evaluation_interval == 1:
             print("\n---- Evaluating Model ----")
             # Evaluate the model on the validation set
             precision, recall, AP, f1, ap_class = evaluate(
